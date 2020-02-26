79447703d315ae9a3e9ba9a20df38a417e42e140
Nathan Nieuwenhuizen <nathan-san@live.nl>
2020-02-26 15:56:40 +0100

    Setup aim mechanic

--- RoboCodeProject/Class1.cs
+++ RoboCodeProject/Class1.cs
@@ -15,10 +15,10 @@ namespace RoboCodeProject
         public override void Run()
         {
             blackBoard.robot = this;
-            BeahaviorTree = new Sequence(blackBoard, 
-                new ScanRobot(blackBoard, 360), 
-                new MoveAhead(blackBoard, 100),
-                new MoveAhead(blackBoard, -100)
+
+BeahaviorTree = new Sequence(blackBoard,
+                new ScanRobot(blackBoard, 360),
+                new TurnGunTowardsScannedTank(blackBoard)
                 );
             IsAdjustGunForRobotTurn = true;
             IsAdjustRadarForGunTurn = true;
@@ -29,9 +29,10 @@ namespace RoboCodeProject
             }
 
         }
+        
         public override void OnScannedRobot(ScannedRobotEvent evnt)
         {
-            Out.WriteLine("I scanned robot: " + evnt.Name);
+            //Out.WriteLine("I scanned robot: " + evnt.Name);
             blackBoard.lastScannedRobotEvent = evnt;
             
         }
--- RoboCodeProject/Tasks/TrunGunTowardsScannedTank.cs
+++ RoboCodeProject/Tasks/TrunGunTowardsScannedTank.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Robocode;
 
 namespace RoboCodeProject
 {
@@ -12,15 +13,45 @@ namespace RoboCodeProject
         {
             this.blackBoard = blackBoard;
         }
+        public double GetRotationToScannedRobot()
+        {
+            return blackBoard.lastScannedRobotEvent.Bearing -
+                    (blackBoard.robot.GunHeading - blackBoard.robot.Heading);
+        }
+        public double GetRotationToScannedRobotFuture()
+        {
+            ScannedRobotEvent enemy = blackBoard.lastScannedRobotEvent;
+
+            double angleToEnemy = enemy.Bearing;
+
+            // Calculate the angle to the scanned robot
+            double angle = (blackBoard.robot.Heading + angleToEnemy % 360);
+
+            // Calculate the coordinates of the robot
+            double enemyX = (blackBoard.robot.X + Math.Sin(angle) * enemy.Distance);
+            double enemyY = (blackBoard.robot.Y + Math.Cos(angle) * enemy.Distance);
+            blackBoard.robot.Out.WriteLine("enemy x: " + enemyX + " | enemy y: " + enemyY);
+            return 0;
+        }
         public override BTNodeStatus Tick()
         {
 
             if (blackBoard.lastScannedRobotEvent != null)
             {
-                double rotation = blackBoard.lastScannedRobotEvent.Bearing -
-                    Math.Abs(blackBoard.robot.GunHeading - blackBoard.robot.Heading);
-                blackBoard.robot.TurnGunRight(rotation);
-                if (Math.Abs(rotation) < 1)
+                GetRotationToScannedRobotFuture();
+                double rotation = GetRotationToScannedRobot();
+
+                if (rotation < 180)
+                {
+                    blackBoard.robot.TurnGunRight(rotation);
+                }
+                else
+                {
+                    blackBoard.robot.TurnGunLeft( 360 - rotation);
+                }
+
+
+                if (Math.Abs(rotation) < 10)
                 {
                     return BTNodeStatus.succes;
                 } else
